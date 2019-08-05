9392da26a9860bf2c0fe4b55eea2fdcd1ac5a035
Gossipboy007 <1066915806@qq.com>
2019-08-05 19:25:23 +0800

    Site updated: 2019-08-05 19:25:20

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Tue May 28 2019 10:58:49 GMT+0800 (GMT+08:00)
+// build time:Mon Aug 05 2019 19:25:19 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Tue May 28 2019 10:58:49 GMT+0800 (GMT+08:00)
+// build time:Mon Aug 05 2019 19:25:19 GMT+0800 (GMT+08:00)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
